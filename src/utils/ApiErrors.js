f476b9e455099bb015cd59f0a51b2df1afad8d7c
Pushkar456 <91177786+Pushkar456@users.noreply.github.com>
2023-11-13 22:28:45 +0530

    add cloudinary and multer for file upload

--- src/utils/ApiErrors.js
+++ src/utils/ApiErrors.js
@@ -1,5 +1,5 @@
 class ApiError extends Error {
-    constructor(statusCode, message = "Something wents wrong", errors = [], statck = "") {
+    constructor(statusCode, message = "Something wents wrong", errors = [], stack = "") {
         super(message);
         this.statusCode = statusCode
         this.message = message
@@ -7,8 +7,8 @@ class ApiError extends Error {
         this.data = null
         this.success = false
 
-        if (statck) {
-            this.stack = statck
+        if (stack) {
+            this.stack = stack
         } else {
             Error.captureStackTrace(this, this.constructor)
         }
